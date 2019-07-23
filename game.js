fe35982608fa744077d125616f6b98d633cbc0fe
Yuri Mello <yuridemeello@gmail.com>
2019-07-22 23:19:38 -0300

    incluindo background

--- game.js
+++ game.js
@@ -1,3 +1,4 @@
+// Cria o canvas
 const canvas = document.createElement('canvas')
 const ctx = canvas.getContext('2d')
 
@@ -5,3 +6,14 @@ canvas.width = 512
 canvas.height = 480
 
 document.body.appendChild(canvas)
+
+
+//Imagem de fundo
+let bgReady = false
+const bgImage = new Image()
+
+bgImage.onload = function() {
+    bgReady = true;
+}
+
+bgImage.src = 'images/background.png'
